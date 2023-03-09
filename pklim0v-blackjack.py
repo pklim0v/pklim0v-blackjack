1a2a0689ceaf86bd57c45a851eee5519227d5b57
pklim0v <pavel.al.klimov@gmail.com>
2023-03-09 18:58:55 +0300

    Initial commit

---
+++ pklim0v-blackjack.py
@@ -0,0 +1,10 @@
+def print_privet(name):
+    print('Hi, {name}!'.format(name=name))
+
+def get_user_name():
+    user_name = input('Hello, friend! Please enter your name: ')
+    return user_name
+
+
+if __name__ == '__main__':
+    print_privet(get_user_name())
