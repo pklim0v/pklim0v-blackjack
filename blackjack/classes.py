0a55f90d0415e9fb2e70c31b4c8af606b89b64a9
pklim0v <pavel.al.klimov@gmail.com>
2023-03-10 23:39:20 +0300

    Added class of Card

--- blackjack/classes.py
+++ blackjack/classes.py
@@ -7,3 +7,18 @@
 class Player:
     def __init__(self, input_name):
         self.name = input_name
+        self.cards = []
+
+
+# class of Card, which have the following attributes:
+#
+#
+#
+#
+
+class Card:
+    def __init__(self, card):
+        self.suit = card[0]
+        self.name = card[1]
+
+
